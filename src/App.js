4b25d890b1028b098882ec08926a43b5bb32072e
Sakib Ahmed <sakibahmed@10-20-55-40.dynapool.wireless.nyu.edu>
2021-09-10 19:21:32 -0400

    Added appbar and routed the tabs to the site pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Tabs, Tab, AppBar} from '@material-ui/core';
 import './App.css';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import Resume from "./components/Resume";
+import Projects from "./components/Projects";
 
 function App() {
+
+  const routes = ["/about","/resume", "/projects"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Route 
+          path="/" 
+          render={(history) => (
+            <AppBar position="sticky">
+              <Tabs value={history.location.pathname !== '/' ? history.location.pathname: false }> 
+                <Tab 
+                  label="About" 
+                  value={routes[0]} 
+                  component={Link} 
+                  to={routes[0]}
+                  />
+                <Tab 
+                  label="Resume" 
+                  value={routes[1]} 
+                  component={Link} 
+                  to={routes[1]}
+                  />
+                <Tab 
+                  label="Projects" 
+                  value={routes[2]} 
+                  component={Link} 
+                  to={routes[2]}
+                  />
+              </Tabs>
+            </AppBar>
+          )}
+        />
+
+        <Switch>
+          <Route path="/resume" component={Resume}/>
+          <Route path="/projects" component={Projects}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
