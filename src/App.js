cf844da8dc18d33dd21d0d25f02e597b9f9bd767
Sakib Ahmed <sakibahmed@10-20-55-40.dynapool.wireless.nyu.edu>
2021-09-12 16:07:00 -0400

    Added grid container and items, inc. an avatar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import {Tabs, Tab, AppBar} from '@material-ui/core';
-import './App.css';
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import {Tabs, Tab, AppBar} from '@material-ui/core';
+import { createTheme,makeStyles } from "@material-ui/core/styles";
 import Resume from "./components/Resume";
 import Projects from "./components/Projects";
+import About from "./components/About";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+    padding: '1%',
+  },
+})
 
 function App() {
-
   const routes = ["/about","/resume", "/projects"];
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div className={classes.root}>
       <BrowserRouter>
         <Route 
           path="/" 
           render={(history) => (
             <AppBar position="sticky">
-              <Tabs value={history.location.pathname !== '/' ? history.location.pathname: false }> 
+              <Tabs variant='fullWidth' value={history.location.pathname !== '/' ? history.location.pathname: false }> 
                 <Tab 
                   label="About" 
                   value={routes[0]} 
@@ -43,6 +51,7 @@ function App() {
         <Switch>
           <Route path="/resume" component={Resume}/>
           <Route path="/projects" component={Projects}/>
+          <Route path="/about" component={About}/>
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import {Card, Grid, Avatar} from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import profilepic from '../portfolioavatar.png';
 
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+    }
+})
 export default function() {
-    return <h1> about </h1>;
+    const classes = useStyles();
+    return (
+        <Grid container spacing={1}>
+            <Grid item xs={12}>
+                <Avatar style={{margin:'20px'}} height={24} width={24} alt="Sakib Ahmed" src={profilepic}> </Avatar>
+                <h1> I'm currently a senior at NYU </h1> 
+            </Grid>
+            <Grid item xs={6}>
+                <h1> Hi </h1>
+            </Grid>
+        </Grid>
+    )
 }
