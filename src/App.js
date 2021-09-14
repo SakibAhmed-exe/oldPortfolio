933a7a4c4f40c874deffb00ed8b9d76e29759100
Sakib Ahmed <sakibahmed@10-20-55-40.dynapool.wireless.nyu.edu>
2021-09-14 12:29:52 -0400

    Styled appbar and added cards to grid

--- src/App.js
+++ src/App.js
@@ -2,17 +2,49 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import {Tabs, Tab, AppBar} from '@material-ui/core';
-import { createTheme,makeStyles } from "@material-ui/core/styles";
+import { createTheme,makeStyles, withStyles } from "@material-ui/core/styles";
 import Resume from "./components/Resume";
 import Projects from "./components/Projects";
 import About from "./components/About";
 
-const useStyles = makeStyles({
+const StyledTabs = withStyles({
+  indicator: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    '& > span': {
+      maxWidth: 80,
+      width: '100%',
+      backgroundColor: 'white',
+    },
+  },
+})((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
+
+const StyledTab = withStyles((theme) => ({
+  root: {
+    textTransform: 'none',
+    color: 'white',
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    '&:focus': {
+      opacity: 1,
+    },
+  },
+}))((props) => <Tab disableRipple {...props} />); 
+
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     padding: '1%',
   },
-})
+  appbar: {
+    marginTop: '1%',
+    background: 'linear-gradient(179deg,#614385 10%, #516395 90%)',
+    boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+    borderRadius: "20px",
+  },
+}));
 
 function App() {
   const routes = ["/about","/resume", "/projects"];
@@ -23,27 +55,27 @@ function App() {
         <Route 
           path="/" 
           render={(history) => (
-            <AppBar position="sticky">
-              <Tabs variant='fullWidth' value={history.location.pathname !== '/' ? history.location.pathname: false }> 
-                <Tab 
+            <AppBar position="sticky" className={classes.appbar}>
+              <StyledTabs className={classes.tabs} variant='fullWidth' value={history.location.pathname !== '/' ? history.location.pathname: false }> 
+                <StyledTab 
                   label="About" 
                   value={routes[0]} 
                   component={Link} 
                   to={routes[0]}
                   />
-                <Tab 
+                <StyledTab 
                   label="Resume" 
                   value={routes[1]} 
                   component={Link} 
                   to={routes[1]}
                   />
-                <Tab 
+                <StyledTab 
                   label="Projects" 
                   value={routes[2]} 
                   component={Link} 
                   to={routes[2]}
                   />
-              </Tabs>
+              </StyledTabs>
             </AppBar>
           )}
         />
--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import {Card, Grid, Avatar} from '@material-ui/core';
+import {Card, CardActions, CardContent, Grid, Avatar, CardMedia, Button, TextField, Typography} from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
 import profilepic from '../portfolioavatar.png';
 
-const useStyles = makeStyles({
-    root: {
-        flexGrow: 1,
-    }
-})
+const useStyles = makeStyles((theme) => ({
+    avatar: {
+        margin: theme.spacing(1)
+      },
+      form: {
+        marginTop: theme.spacing(1),
+        padding: theme.spacing(5)
+      },
+      submit: {
+        margin: theme.spacing(3, 0, 2),
+        background: "#F12D77",
+        color: "#fff"
+      },
+      continue: {
+        margin: theme.spacing(3)
+      },
+      grid: {
+        marginTop: '1%',
+        background: "#fffff",
+        boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+        borderRadius: "20px",
+      },
+      cardmedia: {
+        borderRadius: "20px",
+        margin: '8%',
+        width: "100%",
+		maxHeight: 250,
+		minWidth: 250, 
+      },
+      card: {
+          margin: '10%',
+          marginRight: '7%',
+      },
+    }));
 export default function() {
     const classes = useStyles();
     return (
-        <Grid container spacing={1}>
-            <Grid item xs={12}>
-                <Avatar style={{margin:'20px'}} height={24} width={24} alt="Sakib Ahmed" src={profilepic}> </Avatar>
-                <h1> I'm currently a senior at NYU </h1> 
+        <Grid container className={classes.grid} justifyContent="center" alignItems="center">
+            <Grid item xs={6} s={4} md={3}>
+                <CardMedia title="login" className={classes.cardmedia} image={profilepic} component="img" />
             </Grid>
-            <Grid item xs={6}>
-                <h1> Hi </h1>
+            <Grid item xs={6} s={8} md={9}> 
+                <Card className={classes.card}> 
+                    <CardContent>
+                        <Typography gutterBottom> Sakib Ahmed </Typography> 
+                    </CardContent>
+                </Card>
             </Grid>
         </Grid>
-    )
+  );
 }
