2dc0c923c4673ce2d4c2b17cacf3fe0c8f7aa3bc
Sakib Ahmed <sakibahmed@10-20-55-40.dynapool.wireless.nyu.edu>
2021-09-22 13:23:09 -0400

    Changed pfp and fixed position of contacts

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Card, CardActions, CardContent, Grid, Avatar, CardMedia, Button, TextField, Typography, Tooltip} from '@material-ui/core';
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import profilepic from '../portfolioavatar.png';
+import memojipfp from '../memojipfp.jpg';
 import {DiReact, DiGit, DiJavascript1, DiJava, DiVisualstudio} from "react-icons/di";
 import {SiRedux} from "react-icons/si";
 import Skills from './Skills';
@@ -15,21 +16,25 @@ const useStyles = makeStyles((theme) => ({
         marginTop: '3%',
         background: "#fffff",
         boxShadow: "0px 10px 35px 5px rgba( 155, 164, 193, .8 )",
-        borderRadius: "40px",   
+        borderRadius: "40px",
+        justifyContent:"center",
+        alignItems:"center"
+        
       },
       cardmedia: {
         borderRadius: "75px",
         marginTop: '8%',
         marginBottom: '8%',
-        marginLeft: '0%',
+        margin: 'auto',
         width: "100%",
-		maxHeight: 300,
-        minWidth: 250, 
+        height: '100%',
         boxShadow: "0px 10px 35px 5px rgba( 155, 164, 193, .8 )",
+        maxHeight: '300px',
+        maxWidth: '300px',
       },
       card: {
           alignItems: 'center',
-          background: 'linear-gradient( 35deg, #5EFCE8 10%, #736EFE 100%)',
+          background: 'linear-gradient( 170deg, #76d275 35%, #736EFE 100%)',
           borderRadius: '40px',
           boxShadow: "0px 10px 35px 5px rgba( 155, 164, 193, .8 )",
       },
@@ -58,12 +63,12 @@ export default function() {
     const bull = <span className={classes.bullet}>•</span>;
     return (
       <>
-        <Grid container className={classes.grid} justifyContent="center" alignItems="center">
-            <Grid item xs={6} s={3} md={3}>
-                <CardMedia title="login" className={classes.cardmedia} image={profilepic} component="img" />
+        <Grid container className={classes.grid}>
+            <Grid item xs={12} s={3} md={3}>
+                <CardMedia title="login" className={classes.cardmedia} image={memojipfp} component="img" />
             </Grid>
-            <Grid item xs={6} s={7} md={8}> 
-                <Card style={{marginLeft: '17%',}} className={classes.card}> 
+            <Grid item xs={12} md={7}> 
+                <Card style={{marginLeft: '7%', marginRight: '7%'}} className={classes.card}> 
                     <CardContent>
                         <Typography className={classes.typography} variant='h3'> Hi, I'm Sakib Ahmed </Typography> 
                         <Typography style={{color: '#424242',}} className={classes.typography} variant='h5'> (Suh{bull}kēb)</Typography>
@@ -71,10 +76,10 @@ export default function() {
                     </CardContent>
                 </Card>
             </Grid>
-            <Grid item xs={10} s={10} md={10}> 
-                <Card style={{marginTop: '5%', background:'linear-gradient( 350deg, #5EFCE8 10%, #736EFE 100%)'}} className={classes.card}>
+            <Grid item xs={12} s={10} md={10}> 
+                <Card style={{marginTop: '5%', background:'linear-gradient( 350deg, #76d275 35%, #736EFE 100%)'}} className={classes.card}>
                     <CardContent>
-                        <Typography style={{fontSize: '1.7em'}} className={classes.typography} variant='body1'>
+                        <Typography style={{fontSize: '1.5em'}} className={classes.typography} variant='body1'>
                             I'm currently an undergraduate student at New York University pursuing degrees in Computer 
                             Science and Economics (Theory). I have experience with front end development, UI/UX design, and state 
                             management in an agile environment. My interests include software development, natural langauge processing, and cryptography. 
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,4 +1,4 @@
-import {Tooltip, Button, Grid, withStyles,makeStyles, Zoom, Typography} from '@material-ui/core';
+import {Tooltip, Button, Grid, withStyles,makeStyles, Zoom, Typography, Avatar} from '@material-ui/core';
 import {DiReact, DiGit, DiJavascript1, DiJava, DiVisualstudio, DiCss3, DiPython} from "react-icons/di";
 import {SiRedux, SiJavascript} from "react-icons/si";
 import {AiFillGithub, AiFillLinkedin, AiOutlineTwitter} from "react-icons/ai";
@@ -6,14 +6,16 @@ import {MdEmail} from "react-icons/md";
 
 const useStyles = makeStyles((theme) => ({
     grid: {
-        paddingLeft: '14%',
         padding: '3%',
         marginTop: '3%',
+        margin: 'auto',
         background: "#fffff",
         boxShadow: "0px 10px 35px 5px rgba( 155, 164, 193, .8 )",
         borderRadius: "40px",
-        justifyContent: 'space-evenly',
-        alignItems: 'stretch',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'center',
+        width: '90%',
       },
       button: {
           borderRadius: '15px',
@@ -58,29 +60,25 @@ export default function Contacts() {
     const classes = useStyles();
     return (
         <Grid container className={classes.grid}>
-            <Grid item xs={3} md={3}>
+            <Grid item xs='auto' style={{paddingRight: '5%'}}>
                 <Button target="_blank" href="https://github.com/SakibAhmed-exe" className={classes.button}> 
-                    <AiFillGithub size={'55px'}>
-                    </AiFillGithub>
+                    <AiFillGithub size={'55px'}/>
                 </Button>
             </Grid>
-            <Grid item xs={3} md={3}> 
+            <Grid item xs='auto' style={{paddingRight: '5%', paddingLeft: '5%'}}> 
                 <Button target="_blank" href="https://www.linkedin.com/in/sakibahmed-exe/" lassName={classes.button}> 
-                    <AiFillLinkedin size={'55px'}>
-                    </AiFillLinkedin>
+                    <AiFillLinkedin size={'55px'}/>
                 </Button>
             </Grid>
-            <Grid item xs={3} md={3}> 
+            <Grid item xs='auto' style={{paddingRight: '5%', paddingLeft: '5%'}}> 
                 <Button target="_blank" href="https://twitter.com/Saboyyaboy" className={classes.button}> 
-                    <AiOutlineTwitter size={'55px'}>
-                    </AiOutlineTwitter>
+                    <AiOutlineTwitter size={'55px'}/>
                 </Button>
             </Grid>
-            <Grid item xs={3} md={3}>
+            <Grid item xs='auto' style={{paddingLeft: '5%'}}>
               <LightTooltip TransitionComponent={Zoom} TransitionProps={{timeout: 400}} title="ahmed.sakib@nyu.edu" enterDelay={400} leaveDelay={200}> 
                 <Button className={classes.button}> 
-                    <MdEmail size={'55px'}>
-                    </MdEmail>
+                    <MdEmail size={'55px'}/>
                 </Button>
               </LightTooltip> 
             </Grid>
